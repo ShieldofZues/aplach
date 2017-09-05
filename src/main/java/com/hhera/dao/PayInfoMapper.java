d9a8f12e27d5699c206e4e072ffdc5d0a4741701
[ShieldofZues] <[ShieldofZues@hotmail.com]>
2017-09-05 11:58:32 +0800

    项目初始化第二次提交

---
+++ src/main/java/com/hhera/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.hhera.dao;
+
+import com.hhera.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
